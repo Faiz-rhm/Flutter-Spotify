34102748df3e4c1d031407d3cf692ef8a4f78cda
Rahman <faiz199011@gmail.com>
2022-11-01 21:34:19 +0430

    Add images and refactor UI

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -11,49 +11,49 @@ class _HomePageState extends State<HomePage> {
 
   final List<Map<String, dynamic>> _list = [
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image1.png',
       'title': 'stay',
       'description': 'The Kid LAROI,Justin Bieber',
       'favorite': true,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image2.png',
       'title': 'Wishing Well',
       'description': 'Juice WLRD',
       'favorite': true,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image3.png',
       'title': 'Unstable',
       'description': 'Justin Bieber,The Kid LAROI',
       'favorite': false,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image4.png',
       'title': 'stay',
       'description': 'The Kid LAROI,Justin Bieber',
       'favorite': false,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image1.png',
       'title': 'stay',
       'description': 'The Kid LAROI,Justin Bieber',
       'favorite': true,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image2.png',
       'title': 'Wishing Well',
       'description': 'Juice WLRD',
       'favorite': true,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image3.png',
       'title': 'Unstable',
       'description': 'Justin Bieber,The Kid LAROI',
       'favorite': false,
     },
     {
-      'image': 'assets/images/google.png',
+      'image': 'assets/images/image4.png',
       'title': 'stay',
       'description': 'The Kid LAROI,Justin Bieber',
       'favorite': false,
@@ -65,7 +65,11 @@ class _HomePageState extends State<HomePage> {
     return CustomScrollView(
       slivers: [
         SliverAppBar(
-          surfaceTintColor: Colors.black,
+          stretch: false,
+          snap: true,
+          floating: true,
+          surfaceTintColor: const Color.fromRGBO(0, 0, 0, 1),
+          backgroundColor: Colors.black,
           expandedHeight: 378,
           elevation: 1,
           pinned: true,
@@ -91,11 +95,15 @@ class _HomePageState extends State<HomePage> {
             SizedBox(width: 16),
           ],
           flexibleSpace: FlexibleSpaceBar(
-            collapseMode: CollapseMode.pin,
+            // collapseMode: CollapseMode.parallax,
             background: Container(
-              alignment: Alignment.topLeft,
-              color: Colors.red,
               padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 32),
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/images/image3.png'),
+                  fit: BoxFit.none,
+                )
+              ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.end,
                 crossAxisAlignment: CrossAxisAlignment.start,
@@ -112,7 +120,7 @@ class _HomePageState extends State<HomePage> {
                       SizedBox(width: 4,),
                       Text('2hr 40min', style: TextStyle(color: Colors.white54, fontSize: 13)),
                     ],
-                  )
+                  ),
                 ],
               ),
             ),
@@ -122,14 +130,25 @@ class _HomePageState extends State<HomePage> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              Container(
+                height: 65,
+                width: 65,
+                decoration: BoxDecoration(
+                  color: Colors.green,
+                  borderRadius: BorderRadius.circular(50)
+                ),
+                child: const Icon(Icons.play_arrow, size: 35,),
+
+              ),
               const Padding(
-                padding: EdgeInsets.all(16),
+                padding: EdgeInsets.only(top:16, left: 16, right: 16,),
                 child: Text('Featuring', style: TextStyle(color: Colors.white, fontSize: 24))),
               ListView.separated(
                 separatorBuilder: (context, index) {
-                  return Divider(
-                    height: 1,
-                    color: Colors.grey.shade100,
+                  return const Divider(
+                    height: 0,
+                    thickness: 0.4,
+                    color: Colors.grey,
                   );
                 },
                 shrinkWrap: true,
@@ -138,42 +157,47 @@ class _HomePageState extends State<HomePage> {
                 itemBuilder: (BuildContext context, int index) {
                   final item = _list[index];
 
-                  return Container(
-                    padding: const EdgeInsets.all(16),
+                  return Card(
                     color: Colors.black,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Expanded(
-                          flex: 0,
-                          child: Container(
-                            height: 50,
-                            width: 50,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10)
+                    child: InkWell(
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(16),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Expanded(
+                              flex: 0,
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10)
+                                ),
+                                child: Image.asset(item['image']),
+                              ),
                             ),
-                            child: Image.asset(item['image']),
-                          ),
+                            const SizedBox(width: 12,),
+                            Expanded(
+                              flex: 3,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(item['title'].toString().toUpperCase(), style: const TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w700)),
+                                  const SizedBox(height: 8),
+                                  Text(item['description'], style: const TextStyle(fontSize: 12, color: Colors.white, fontWeight: FontWeight.w500))
+                                ],
+                              ),
+                            ),
+                            Icon(Icons.favorite, color: item['favorite']
+                              ? Colors.green
+                              : Colors.white),
+                            const SizedBox(width: 12),
+                            const Icon(Icons.more_vert, color: Colors.white,)
+                          ],
                         ),
-                        const SizedBox(width: 12,),
-                        Expanded(
-                          flex: 3,
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(item['title'].toString().toUpperCase(), style: const TextStyle(fontSize: 15, color: Colors.white, fontWeight: FontWeight.w700)),
-                              const SizedBox(height: 8),
-                              Text(item['description'], style: const TextStyle(fontSize: 12, color: Colors.white, fontWeight: FontWeight.w500))
-                            ],
-                          ),
-                        ),
-                        Icon(Icons.favorite, color: item['favorite']
-                          ? Colors.green
-                          : Colors.white),
-                        const SizedBox(width: 12),
-                        const Icon(Icons.more_vert, color: Colors.white,)
-                      ],
+                      ),
                     ),
                   );
                 },
