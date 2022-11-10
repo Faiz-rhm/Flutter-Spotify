a044d2efc4a14d1727977a8e0a3c59bed2bfba51
Rahman <faiz199011@gmail.com>
2022-11-10 19:59:25 +0430

    Add Login Page

--- lib/view/get_started_page.dart
+++ lib/view/get_started_page.dart
@@ -15,17 +15,16 @@ class GetStartedPage extends StatelessWidget {
             image: AssetImage('assets/images/getStarted.png'),
             fit: BoxFit.cover
           ),
-
         ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Text('A digital music, podcast, and video service that gives you access to millions of songs and other content from creators all over the world.',
-              style: TextStyle(color: ColorConstants.starterWhite, fontSize: 16, fontWeight: FontWeight.w600, ),
+              style: TextStyle(color: ColorConstants.starterWhite, fontSize: 17, fontWeight: FontWeight.w600, ),
               textAlign: TextAlign.center,
             ),
-            const SizedBox(height: 16,),
+            const SizedBox(height: 32,),
             MaterialButton(
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(31)),
               height: 58,
@@ -33,6 +32,7 @@ class GetStartedPage extends StatelessWidget {
               onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => const LoginPage())),
               child: const Text('Get Started', style: TextStyle(color: Colors.white, fontSize: 18),) ,
             ),
+            const SizedBox(height: 32,)
           ],
         ),
       ),
