74f96f77912c63089447b275ff67b38be0c3d362
Rahman <faiz199011@gmail.com>
2022-11-22 08:49:19 +0430

    Add data

---
+++ lib/view/data/data.dart
@@ -0,0 +1,38 @@
+   final List<Map<String, dynamic>> slider = [
+      {
+        "title" : "30 Fresh music for you every week ",
+        "image" : "assets/images/yourDiscover.png",
+      },
+      {
+        "title" : "New songs every friday",
+        "image" : "assets/images/yourDiscover.png",
+      },
+   ];
+
+   final List<Map<String, dynamic>> topMixes = [
+      {
+        "title" : "Hip Hop Mix",
+        "description" : "Juice Wrld, Drake, Kendrick lamar and more... ",
+        "image" : "assets/images/music1.png",
+        "color" : "#EF0CAF"
+      },
+      {
+        "title" : "Moody Mix",
+        "description" :  "Joji, The Kid LAROI, Tate McRae and more...",
+        "image" : "assets/images/music2.png",
+        "color" : "#FFFF00"
+      },
+      {
+        "title" : "House Mix",
+        "description" :  "Martin Garrix, The Chainsmoker and more...",
+        "image" : "assets/images/music3.png",
+        "color" : "#1ED760"
+      },
+   ];
+
+   List<String> artists = [
+    'assets/images/artist1.png',
+    'assets/images/artist2.png',
+    'assets/images/artist3.png',
+   ];
+
