aec0c81eef19f076de8d8c22af4db11f5d36d4de
Rahman <faiz199011@gmail.com>
2022-11-24 08:41:21 +0430

    update data

--- lib/view/data/data.dart
+++ lib/view/data/data.dart
@@ -30,9 +30,45 @@
       },
    ];
 
-   List<String> artists = [
-    'assets/images/artist1.png',
-    'assets/images/artist2.png',
-    'assets/images/artist3.png',
+   List<Map<String, dynamic>> artists = [
+    {
+      'image': 'assets/images/artist1.png',
+      'name' : 'The Kid LAROI'
+    },
+    {
+      'image':  'assets/images/artist2.png',
+      'name': 'Taylor Swift'
+    },
+    {
+      'image': 'assets/images/artist3.png',
+      'name': 'Post Malone'
+    }
    ];
 
+   List<String> searchList = [
+    'The Kid LAROI',
+    'Drake',
+    'Justin Bieber',
+    'Joji',
+    'Hip Hop',
+    'Pop',
+    'Top-Hits'
+   ];
+
+final List<Map<String, dynamic>> podCast = [
+      {
+        "title" : "Impaulsive",
+        "description" : "334 ep.",
+        "image" : "assets/images/impaulsive.png",
+      },
+      {
+        "title" : "TED Talks Daily",
+        "description" : "155 ep.",
+        "image" : "assets/images/tedTalk.png",
+      },
+      {
+        "title" : "Mom’s Basement",
+        "description" : "75 ep.",
+        "image" : "assets/images/momsBasement.png",
+      },
+   ];
